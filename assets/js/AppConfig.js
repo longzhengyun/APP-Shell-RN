673ad62baaab9e19435b70ce6efeb806805342f5
Jary <jary0419@163.com>
2019-04-04 14:32:20 +0800

    update

--- assets/js/AppConfig.js
+++ assets/js/AppConfig.js
@@ -1,7 +1,7 @@
 import { version } from './../../package.json';
 
 const apiOrigin = 'http://192.168.20.13:8097/api'
-const origin = 'http://192.168.10.177:3000';
+const origin = 'http://192.168.10.191:3000';
 
 const updateOrigin = ``
 
